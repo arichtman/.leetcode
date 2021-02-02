fea20c8066152376795c8150c843a291e118b3bd
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2021-02-03 07:25:16 +1030

    iterative solution

--- 206.reverse-linked-list.py
+++ 206.reverse-linked-list.py
@@ -12,6 +12,23 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        
+        pastNode = None
+        currentNode = head
+        while currentNode is not None:
+          nextNode = currentNode.next
+          currentNode.next = pastNode
+          pastNode = currentNode
+          currentNode = nextNode
+        return pastNode
+
 # @lc code=end
+
+Pseudo
+
+get next node reference
+set head node next to null
+set next node reference to head
+move to next node
+store next node reference
+update object to point to previous node
 
