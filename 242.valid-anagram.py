fa4cfbc3565e7c5b124ff356df2d870abb9b9a0d
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2021-02-26 06:33:46 +1030

    e z

---
+++ 242.valid-anagram.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) == len(t):
+            return sorted(s) == sorted(t)
+        return False
+        
+# @lc code=end
+
+s = 'a'
+t = 'b'
+
+s = "anagram"
+t = "nagaram"
+list(s).sort()
+list(t).sort()
