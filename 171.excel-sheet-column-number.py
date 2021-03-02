07f9aa1a11ea48e89a3c5e045b60928b23fc98f3
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2021-03-03 06:36:36 +1030

    added pythonic solution

--- 171.excel-sheet-column-number.py
+++ 171.excel-sheet-column-number.py
@@ -11,4 +11,5 @@ class Solution:
         for index, char in enumerate(s[::-1]):
             total += (ord(char)-64)*pow(26, index)
         return total
+        # return sum(map(lambda x: (ord(x[1])-64)*pow(26, x[0]), enumerate(s[::-1])))
 # @lc code=end
